c86a0afee9d195fd0a6bac82726de14fca0d7d7f
Dennis Hedegaard <neo2k@neo2k.dk>
2010-03-11 18:24:31 +0100

    a sort of working setshellpath implementaiton, at least on linux

--- main.c
+++ main.c
@@ -16,12 +16,12 @@ void setshellpath(char *arg0);
 int main(int argc, char **argv, char **env) {
 	char input[MAX_LINE_LENGTH];
 	printf("sash shell revision: %d\n", REVISION);
-	/* setshellpath(argv[0]); */
+	setshellpath(argv[0]);
 	setenviron(env);
 	while (1) {
 		printf("%s", PROMPT);
 		if (fgets(input, MAX_LINE_LENGTH, stdin) == NULL) {
-			fprintf(stderr, "EOF caught, quitting.\n");
+			fprintf(stderr, "\nEOF caught, quitting.\n");
 			exit(1);
 		}
 		parse(input);
@@ -30,13 +30,23 @@ int main(int argc, char **argv, char **env) {
 }
 
 void setshellpath(char *arg0) {
-	/* TODO fix
-	 * perhaps use getcwd() + argv[0] and parse '..' and '.'.
-	 **/
-	/*	char *c, *cwd = getcwd(NULL, 0);
-	 int len = strlen(cwd) + strlen(arg0) + 1 + 4, i = 4;
-	 c = malloc(sizeof(*c) * len);
-	 memcpy(c, "PWD=", 4);
-	 setenv("test=test");
-	 free(c);*/
+	/* TODO fix */
+	char *pid, *newshell;
+	pid = malloc(sizeof(*pid) * 17);
+	if (snprintf(pid, 16, "/proc/%d/exe", getpid()) < 0) {
+		fprintf(stderr,
+				"Unable to get pid, in less than 7 (16 for the rest) chars.\n");
+		exit(1);
+	}
+	printf("pid: %s\n", pid);
+	newshell = malloc(sizeof(*newshell) * 255);
+	if (readlink(pid, newshell, 254) == -1) {
+		fprintf(stderr, "Unable to read executable path from: %s\n", pid);
+		exit(1);
+	}
+	if (setenv("SHELL", newshell, 1) == -1) {
+		fprintf(stderr, "Unable to set the shell to: %s\n", newshell);
+	}
+	free(pid);
+	free(newshell);
 }
