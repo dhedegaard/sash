172b6ab67d9d963a9f7e0ccc8ad28ccc4fc9459d
Dennis Hedegaard <neo2k@neo2k.dk>
2010-03-22 10:59:09 +0100

    cleanup, added test code to comments, optimized trim() more and reworked startswith().

--- utility.c
+++ utility.c
@@ -14,78 +14,73 @@
 
 #include "utility.h"
 
-/**
- * Allocates and makes a new string based on the input from trim.
- * It always allocates 1 extra byte, for terminating the string (\0).
+/* tested with:
+ int main() {
+ assert(trim(NULL) == NULL);
+ assert(strcmp(trim("  "), "") == 0);
+ assert(strcmp(trim("hej"), "hej") == 0);
+ assert(strcmp(trim(" hej"), "hej") == 0);
+ assert(strcmp(trim("hej "), "hej") == 0);
+ assert(strcmp(trim(" hej "), "hej") == 0);
+ assert(strcmp(trim("   eqwqwe  eqw     "), "eqwqwe  eqw") == 0);
+ assert(strcmp(trim("  hej  "), "hej") == 0);
+ assert(strcmp(trim(" hej  "), "hej") == 0);
+ return 0;
  */
-/* static char* makestring(int left, int right, const char *string); */
-/**
- * Checks to see if a char is "valid", meaning it shouldn't get trimmed.
- */
-/* static int checkvalid(char c); */
-
 char* trim(const char *str) {
-        char *result = NULL;
-        const char *strp;
-        int len;
-        if (str == NULL)
-                return NULL;
-        while (isspace(*str)) {
-                if (*str == '\0')
-                        return NULL;
-                str++;
-        }
-        if (*str == '\0') {
-                result = malloc(1);
-                *result = '\0';
-                return result;
-        }
-        strp = str;
-        while (*strp != '\0')
-                strp++;
-        if (*strp == '\0')
-                strp--;
-        while (isspace(*strp) || *strp == '\n')
-                strp--;
-        len = strp - str + 1;
-        result = calloc(sizeof(*result), len + 1);
-        memcpy(result, str, len);
-        *(result + len) = '\0';
-        return result;
+	char *result = NULL;
+	const char *strp;
+	int len;
+	if (str == NULL)
+		return NULL;
+	while (isspace(*str))
+		str++;
+	if (*str == '\0') {
+		result = malloc(1);
+		*result = '\0';
+		return result;
+	}
+	strp = str;
+	while (*strp != '\0')
+		strp++;
+	if (*strp == '\0')
+		strp--;
+	while (isspace(*strp) || *strp == '\n')
+		strp--;
+	len = strp - str + 1;
+	result = calloc(sizeof(*result), len);
+	memcpy(result, str, len);
+	*(result + len) = '\0';
+	return result;
 }
 
-/* The old trim implementation. */
-/* char* trim(const char *string) {
-	int left = 0, right = 0, i;
-	if (string == NULL)
-		return NULL;
-	right = strlen(string) - 1;
-	for (i = 0; i <= right; i++)
-		if (!checkvalid(string[i]))
-			left = i + 1;
-		else
-			break;
-	for (i = right; i >= left; i--)
-		if (!checkvalid(string[i]))
-			right = i - 1;
-		else
-			break;
-	if (left >= right)
-		return NULL;
-	else
-		return makestring(left, right, string);
-} */
-
+/**
+ * Tested with:
+ int main() {
+ assert(startswith(NULL, NULL) == 0);
+ assert(startswith("hej", NULL) == 0);
+ assert(startswith(NULL, "hej") == 0);
+ assert(startswith("hejsa", "hej") == 1);
+ assert(startswith("davs hej", "davs ") == 1);
+ assert(startswith("dav", "dav") == 1);
+ assert(startswith("dav", "davs") == 0);
+ return 0;
+ }
+ */
 int startswith(const char *string, const char *startwith) {
-	int stringlen, startwithlen, i;
-	if ((stringlen = strlen(string)) < (startwithlen = strlen(startwith)))
+	if (string == NULL || startwith == NULL)
 		return 0;
-	for (i = 0; i < startwithlen; i++)
-		if (string[i] != startwith[i])
-			return 0;
-	return startwithlen;
+	while (*string == *startwith) {
+		if (*string == '\0')
+			return 1;
+		string++;
+		startwith++;
+	}
+	if (*startwith == '\0')
+		return 1;
+	else
+		return 0;
 }
-
 char* removefirstword(const char *string) {
 	char *pos;
 	if ((pos = strchr(string, ' ')) != NULL)
@@ -133,18 +128,3 @@ void patherrorhandling(const char *path) {
 		break;
 	}
 }
-
-/* TODO clean */
-/*
-static char* makestring(int left, int right, const char *string) {
-	int len = strlen(string), i, pos = 0;
-	char *newstr = malloc(len);
-	for (i = left; i <= right; i++)
-		newstr[pos++] = string[i];
-	newstr[pos] = '\0';
-	return newstr;
-} */
-
-/* static int checkvalid(char c) {
-	return (!isspace(c) && c != '\n') ? 1 : 0;
-} */
